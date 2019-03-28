f90847f483ae42217bab7436a23c258244285ca2
eHeuristic <eheuristic@eHeuristics-iMac.local>
2019-03-28 10:01:41 +0530

    Initial Commit

---
+++ chatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by eHeuristic on 28/03/19.
+//  Copyright © 2019 eHeuristic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
